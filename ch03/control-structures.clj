61ff21989853a9ac8732426fb865e3b0d070476e
Victor Teodoro <victorjst@hotmail.com>
2019-10-19 02:27:50 -0300

    ch03: add more examples

--- ch03/control-structures.clj
+++ ch03/control-structures.clj
@@ -1,5 +1,9 @@
 (if false
-    (do (println "Show!")
-        "Daorinha")
-    (do (println "Deu ruim!")
-        "Aí não é legal"))
+  (do (println "Show!")
+      "Daorinha")
+  (do (println "Deu ruim!")
+      "Aí não é legal"))
+
+(if nil "nil is truthy" "nil is falsy")
+
+(println (= true true))
